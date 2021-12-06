b8ad2959d962ee9d54eba9116119b4355df758a9
Ilya Ivanenko <ilya__ivanenko@mail.ru>
2021-12-06 18:38:28 +0300

    fix: removed the table header at 0 users

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -2,15 +2,12 @@ import React, { useState } from 'react'
 import api from '../api'
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
-  // console.log(users)
+
   const hadleDelete = (userId) => {
-    // console.log(userId)
     setUsers((prevState) => prevState.filter((user) => user._id !== userId))
   }
+
   const renderPhrase = (number) => {
-    // console.log(number)
-    if (number === 0)
-      return <span className="badge bg-danger">Никто с тобой не тусанет</span>
     const correctVerbForm = number > 1 ? 'тусанут' : 'тусанёт'
     const correctDeclensionOfWord =
       [2, 3, 4].includes(number % 10) && (number < 10 || number > 20)
@@ -20,6 +17,38 @@ const Users = () => {
     return <span className="badge bg-primary">{phrase}</span>
   }
 
+  const renderQualities = (user) => {
+    return user.qualities.map((quality) => {
+      const classes = `badge m-1 bg-${quality.color}`
+      return (
+        <span key={quality._id} className={classes}>
+          {quality.name}
+        </span>
+      )
+    })
+  }
+
+  const renderUsers = () => {
+    return users.map((user) => (
+      <tr key={user._id} onClick={() => hadleDelete(user._id)}>
+        <td>{user.name}</td>
+        <td>{renderQualities(user)}</td>
+        <td>{user.profession.name}</td>
+        <td>{user.completedMeetings}</td>
+        <td>{user.rate} / 5</td>
+        <td>
+          <button className="btn btn-danger">delete</button>
+        </td>
+      </tr>
+    ))
+  }
+
+  if (users.length === 0)
+    return (
+      <h2>
+        <span className="badge bg-danger">Никто с тобой не тусанет</span>
+      </h2>
+    )
   return (
     <>
       <h2>{renderPhrase(users.length)}</h2>
@@ -34,29 +63,7 @@ const Users = () => {
             <th scope="col"></th>
           </tr>
         </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id} onClick={() => hadleDelete(user._id)}>
-              <td>{user.name}</td>
-              <td>
-                {user.qualities.map((quality) => {
-                  const classes = `badge m-1 bg-${quality.color}`
-                  return (
-                    <span key={quality._id} className={classes}>
-                      {quality.name}
-                    </span>
-                  )
-                })}
-              </td>
-              <td>{user.profession.name}</td>
-              <td>{user.completedMeetings}</td>
-              <td>{user.rate} / 5</td>
-              <td>
-                <button className="btn btn-danger">delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{renderUsers()}</tbody>
       </table>
     </>
   )
